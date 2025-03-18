b5e52081029968f3d0e67a39a4327efb122d0bbe
rayray39 <raynerlimfy@gmail.com>
2025-03-18 15:18:48 +0800

    App.tsx: Add modal when new post button clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
+import Modal from "@mui/material/Modal"
 import Paper from "@mui/material/Paper"
 import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+import { useState } from "react"
 
 function App() {
+    const [openNewPostModal, setOpenNewPostModal] = useState<boolean>(false);
+
+    const modalBoxStyle = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: '50%',
+        bgcolor: 'background.paper',
+        boxShadow: 24,
+        p: 4,
+        borderRadius:4
+      };
 
     const addNeWPost = () => {
         console.log('adding new post...');
+        setOpenNewPostModal(true);
     }
 
     const goToMainPage = () => {
@@ -35,6 +52,24 @@ function App() {
                     <Button variant="contained" disableElevation sx={{
                         bgcolor:'mediumseagreen'
                     }} onClick={addNeWPost}>+New Post</Button>
+                    <Modal
+                        disableEnforceFocus
+                        disableAutoFocus
+                        open={openNewPostModal}
+                        onClose={() => setOpenNewPostModal(false)}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={modalBoxStyle}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Text in a modal
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                        </Typography>
+                        </Box>
+                    </Modal>
+
                     <Button sx={{
                         color:'black',
                         '&:hover':{
