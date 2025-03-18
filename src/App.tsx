3eb31f3b21210a0a3cc8ea6deba8c65d592e02fd
rayray39 <raynerlimfy@gmail.com>
2025-03-18 14:35:55 +0800

    App.tsx: Add skeletal structure to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import Box from "@mui/material/Box"
+import Paper from "@mui/material/Paper"
+import Stack from "@mui/material/Stack"
 
 function App() {
     return <>
-    
+        <Stack sx={{
+            maxWidth:'50%',
+            transform:'translateX(50%)',
+            marginTop:'20px'
+        }}>
+            <Paper elevation={3}>
+                This is some content
+            </Paper>
+
+            <Box sx={{
+                textAlign:'center',
+            }}>
+                <h1>Journal your feelings here</h1>
+                <h3>Write what you feel. No account needed!</h3>
+            </Box>
+        </Stack>
     </>
 }
 
