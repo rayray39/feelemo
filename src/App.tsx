b0e52bd38816eb7a993a5ece8af4a9669925bce0
rayray39 <raynerlimfy@gmail.com>
2025-03-23 00:26:59 +0800

    Install react-router-dom for page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import { useState } from "react"
-import Toolbar from "./Toolbar"
-import Stack from "@mui/material/Stack"
-import JournalEntries from "./JournalEntries";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
-    const [journalEntries, setJournalEntries] = useState<string[][]>([]);
 
-    const addJournalEntry = (newEntry:string[]) => {
-        // passed into Toolbar for adding a new journal entry
-        setJournalEntries(prev => [...prev, newEntry]);
-    }
-
-    return <Stack sx={{
-        maxWidth:'50%',
-        transform:'translateX(50%)',
-        marginTop:'20px'
-    }}>
-        <Toolbar addJournalEntry={addJournalEntry} />
-        <JournalEntries entries={journalEntries} />
-    </Stack>    
+    return <>
+        <Router>
+            <Routes>
+                <Route path='/' element={<Home />}/>
+            </Routes>
+        </Router>
+    </>    
 }
 
 export default App
