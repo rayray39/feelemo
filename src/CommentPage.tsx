c3cc0448416b646f822de1b3392827398d65517c
rayray39 <raynerlimfy@gmail.com>
2025-03-24 23:29:06 +0800

    CommentPage.tsx: Add header comments

--- src/CommentPage.tsx
+++ src/CommentPage.tsx
@@ -1,7 +1,8 @@
 import { useParams } from "react-router-dom"
 
 function CommentPage() {
-    const { id } = useParams();
+    // comments page for the journal entry with id
+    const { id } = useParams();     // extract the 'id' field from the URL
 
     return <>
         <h1>{`this is the comments page for ${id}`}</h1>
