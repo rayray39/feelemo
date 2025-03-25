921566fbdb974520971a44e1fe75fe959668db56
rayray39 <raynerlimfy@gmail.com>
2025-03-25 21:29:01 +0800

    CommentPage.tsx: Add like button to each comment

--- src/CommentPage.tsx
+++ src/CommentPage.tsx
@@ -11,22 +11,34 @@ import Typography from "@mui/material/Typography";
 function CommentPage() {
     // comments page for the journal entry with id
     // const { id } = useParams();     // extract the 'id' field from the URL
-    const [newComment, setNewComment] = useState<string>('');
-    const [comments, setComments] = useState<string[]>([]);
+    const [newCommentContent, setNewCommentContent] = useState<string>('');
+    const [comments, setComments] = useState<(number | string)[][]>([]);
 
     // backend instructions
     // 1. load the existing comments in table into comments (useEffect, get request)
 
     const addComment = () => {
-        console.log(`adding new comment: ${newComment}`);
-        if (!newComment) {
+        console.log(`adding new comment: ${newCommentContent}`);
+        if (!newCommentContent) {
             return;
         }
+        const newComment = [newCommentContent, 0];
+        // each new comment is [content, numOfLikes]
         setComments(prev => [...prev, newComment]);
-        setNewComment('');
+        setNewCommentContent('');
 
         // backend instructions
         // 1. add new comment into table (post request)
+    }
+
+    const handleLike = (index:number) => {
+        // when the heart button is clicked on in the Comment card
+        console.log(`liked card id: ${index}`);
+        
+        // updates the num of likes for the comment
+        setComments(prev => prev.map((comment, i) => 
+            i === index ? [comment[0], (comment[1] as number) + 1] : comment
+        ))
     }
 
     return <Stack sx={{
@@ -38,11 +50,11 @@ function CommentPage() {
         <>
             <TextField
                 id="outlined-multiline-static"
-                value={newComment}
-                onChange={(event) => setNewComment(event.target.value)}
+                value={newCommentContent}
+                onChange={(event) => setNewCommentContent(event.target.value)}
                 label="Leave a comment! (No account needed)"
                 multiline
-                rows={4}
+                rows={2}
                 fullWidth
                 sx={{
                     marginTop: '20px',
@@ -62,21 +74,50 @@ function CommentPage() {
                 marginTop:'50px'
             }}>
                 {comments.map((comment, index) => (
-                    <Comment key={index} content={comment} />
+                    <Comment key={index} handleLike={handleLike} index={index} content={comment[0]} numOfLikes={comment[1]} />
                 ))}
             </Stack>
         </>
     </Stack>
 }
 
-function Comment({ content }:{ content:string }) {
+function Comment({ 
+    // represents each comment card
+    handleLike, index, content, numOfLikes }:
+    { 
+        handleLike: (index:number) => void,
+        index:number, 
+        content:(string | number),
+        numOfLikes:(string | number) 
+    }) {
+
+    const addLike = () => {
+        // calls handleLike in CommentPage
+        handleLike(index);
+    }
+    
     return <>
-        <Card>
+        <Card sx={{
+            display:'flex',
+            justifyContent:'space-between',
+            alignItems:'center',
+            padding:'5px'
+        }}>
             <CardContent>
                 <Typography variant="body2" sx={{ color: 'text.secondary' }}>
                     {content}
                 </Typography>
             </CardContent>
+            <Stack sx={{
+                display:'flex',
+                flexDirection:'column',
+                alignItems:'center'
+            }}>
+                <Button disableElevation onClick={addLike} sx={{
+                    maxHeight:'100%',
+                }}>❤️</Button>
+                <>{`${numOfLikes}🤍`}</>
+            </Stack>
         </Card>
     </>
 }
