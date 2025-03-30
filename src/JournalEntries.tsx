7a2d55cc076cc1d0d8ba3940c62e3ae20b5f7045
rayray39 <raynerlimfy@gmail.com>
2025-03-30 22:30:25 +0800

    JournalEntries.tsx: Add margin to the bottom

--- src/JournalEntries.tsx
+++ src/JournalEntries.tsx
@@ -3,7 +3,9 @@ import JournalEntry from "./JournalEntry"
 
 function JournalEntries({ entries }:{ entries:(number | string)[][] }) {
     return <>   
-        <Stack spacing={2}>
+        <Stack spacing={2} sx={{
+            marginBottom:'40px'
+        }}>
             {entries.map((entry, index) => (
                 <JournalEntry key={index} cardIndex={index} entry={entry}/>
             ))}      
